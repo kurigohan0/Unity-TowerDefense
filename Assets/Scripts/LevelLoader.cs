c092f7ff5508b1c7fb4e1fa4254a4b9a18e932fd
Евгений Насонов <kurigohan0@yandex.ru>
2019-11-22 15:07:30 +0300

    Release

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadLevel1()
+    {
+        SceneManager.LoadScene("1");
+
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
